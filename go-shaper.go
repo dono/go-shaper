edecd8d719468a94261df10a5f928580092df1e0
Daichi Ono <ono@DaichinoMacBook-Pro.local>
2019-05-30 11:10:26 +0900

    add -n

--- go-shaper.go
+++ go-shaper.go
@@ -1,57 +1,57 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"strings"
-	"time"
+  "context"
+  "fmt"
+  "log"
+  "strings"
+  "time"
 
-	"github.com/atotto/clipboard"
+  "github.com/atotto/clipboard"
 )
 
 func watch(interval time.Duration) (<-chan string, context.CancelFunc) {
-	var (
-		ch          = make(chan string)
-		lastContent string
-		ctx, cancel = context.WithCancel(context.Background())
-	)
+  var (
+    ch          = make(chan string)
+    lastContent string
+    ctx, cancel = context.WithCancel(context.Background())
+  )
 
-	go func() {
-		defer close(ch)
+  go func() {
+    defer close(ch)
 
-		for {
-			time.Sleep(interval)
+    for {
+      time.Sleep(interval)
 
-			select {
-			case <-ctx.Done():
-				return
-			default:
-			}
+      select {
+      case <-ctx.Done():
+        return
+      default:
+      }
 
-			content, err := clipboard.ReadAll()
-			if err != nil || content == "" || content == lastContent {
-				continue
-			}
+      content, err := clipboard.ReadAll()
+      if err != nil || content == "" || content == lastContent {
+        continue
+      }
 
-			lastContent = content
-			ch <- content
-		}
-	}()
+      lastContent = content
+      ch <- content
+    }
+  }()
 
-	return ch, cancel
+  return ch, cancel
 }
 
 func main() {
-	fmt.Println("running...")
+  fmt.Println("running...")
 
-	ch, _ := watch(200 * time.Millisecond)
-	rep := strings.NewReplacer("\n", " ")
+  ch, _ := watch(200 * time.Millisecond)
+  rep := strings.NewReplacer("-\n", "", "\n", " ")
 
-	for text := range ch {
-		renew := rep.Replace(text)
-		if err := clipboard.WriteAll(renew); err != nil {
-			log.Fatal(err)
-		}
-	}
+  for text := range ch {
+    renew := rep.Replace(text)
+    if err := clipboard.WriteAll(renew); err != nil {
+      log.Fatal(err)
+    }
+  }
 }
